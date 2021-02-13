a67141705eccf9d6d6f7784f5adc60ab64ab9890
zgks <kingtranssun@gmail.com>
2021-02-13 17:58:09 +1100

    Class 3 Homework

--- Homework/Class 3/Quiz4.java
+++ Homework/Class 3/Quiz4.java
@@ -1,6 +1,28 @@
 // Leetcode.22 括号生成
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-    	// Do not find out a solution
+
+    ArrayList<String> resultList = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        processMethod(n, n, "");
+        return resultList;
     }
+
+    public void processMethod (int leftTotal, int rightTotal, String current) {
+        if (leftTotal == 0 && rightTotal == 0) {
+            resultList.add(current);
+            return;
+        }
+
+        if (leftTotal == rightTotal) {
+            processMethod(leftTotal - 1, rightTotal, current +"(");
+        }
+        if (leftTotal < rightTotal){
+            if (leftTotal > 0) {
+                processMethod(leftTotal - 1, rightTotal, current +"(");
+            } 
+            processMethod(leftTotal, rightTotal - 1, current +")");
+        }
+    }
+
 }

ee6da614bdcde5843b3bf2813bfc16f003e06873
Bangjing Yang <962391470@qq.com>
2021-02-15 15:50:40 +1100

    Update 0206.py
    LC1590

--- mxc006_homework/0206.py
+++ mxc006_homework/0206.py
@@ -39,3 +39,40 @@ def characterReplacement(s: str, k: int) -> int:
         right += 1
 
     return right - left
+
+    
+# LC1590
+# 滑动窗口 暴力 遍历全部子链 超出时间
+def minSubarray( nums , p: int) -> int:
+    total = sum(nums)
+    print('total',total)
+    minSub = 1000000
+    
+    if total % p == 0:
+        return 0
+    
+    left, right = 0, 0
+    n = len(nums)
+    for left in range(n):
+        print("")
+        print('left',left)
+        
+        right = left
+        while right < n:
+            print('right',right)
+            
+            subSum = sum(nums[left:right+1])
+            print('subSum',subSum)
+            if subSum == total:
+                break
+            print('total - subSum',total - subSum)
+            if (total - subSum)% p == 0:
+                if right-left+1 == 1:
+                    return 1
+                minSub = min(right-left+1, minSub)
+            right += 1
+        
+    if minSub != 1000000:
+        return minSub
+    return -1
+    

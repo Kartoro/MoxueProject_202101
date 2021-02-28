d5ddf3f047f4c697d3d0a7fcaf9a37467908c79c
Bongtaro Hao <bongtaro13@gmail.com>
2021-02-28 16:20:55 +0800

    844 finished

---
+++ homework/H_20210224.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class H_20210224 {
+    public static void main(String[] args) {
+
+
+    }
+
+    //lc 844
+    public boolean backspaceCompare(String S, String T) {
+        Stack<Character> s1 = new Stack<>();
+        Stack<Character> s2 = new Stack<>();
+
+        char[] sChars = S.toCharArray();
+        char[] tChars = T.toCharArray();
+
+        for (int i = 0; i < sChars.length; i++) {
+            if (sChars[i] == '#') {
+                if (!s1.isEmpty()) {
+                    s1.pop();
+                }
+            } else {
+                s1.push(sChars[i]);
+            }
+        }
+
+        for (int j = 0; j < tChars.length; j++) {
+            if (tChars[j] == '#') {
+                if (!s2.isEmpty()) {
+                    s2.pop();
+                }
+            } else {
+                s2.push(tChars[j]);
+            }
+        }
+
+        List<Character> s1List = new ArrayList<Character>(s1);
+        List<Character> s2List = new ArrayList<Character>(s2);
+
+        if (s1.size() != s2.size()) {
+            return false;
+        }
+
+        for (int k = 0; k < s1List.size(); k++) {
+            if (s1List.get(k) != s2List.get(k)) {
+                return false;
+            }
+        }
+
+        return true;
+
+
+    }
+}

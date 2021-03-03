49840b27a31d23dd256c3e607ab55ed880268409
Bongtaro Hao <bongtaro13@gmail.com>
2021-03-03 14:59:24 +0800

    42 finished

--- homework/H_20210224.java
+++ homework/H_20210224.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class H_20210224 {
     public static void main(String[] args) {
@@ -73,6 +71,153 @@ public class H_20210224 {
         return sb.toString();
 
     }
+
+    //lc 933
+    class RecentCounter {
+
+
+        Queue<Integer> q = new LinkedList<Integer>();
+
+        public RecentCounter() {
+
+        }
+
+        public int ping(int t) {
+            q.offer(t);
+            while (q.peek() < t - 3000) {
+                q.poll();
+            }
+            return q.size();
+
+        }
+    }
+
+    //lc 394
+    public String decodeString(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> sInteger = new Stack<>();
+        Stack<String> sString = new Stack<>();
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                count = count * 10 + s.charAt(i) - '0';
+            } else if (s.charAt(i) == '[') {
+                sInteger.push(count);
+                count = 0;
+                sString.push(sb.toString());
+                sb = new StringBuilder();
+            } else if (s.charAt(i) == ']') {
+                int n = sInteger.pop();
+                String tmp = sb.toString();
+                for (int j = 1; j < n; j++) {
+                    sb.append(tmp);
+                }
+                sb = new StringBuilder(sString.pop() + sb.toString());
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    //lc 994
+    public int orangesRotting(int[][] grid) {
+        int l = grid.length;
+        int w = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+
+        int fresh = 0;
+        for (int m = 0; m < l; m++) {
+            for (int n = 0; n < w; n++) {
+                if (grid[m][n] == 2) {
+                    q.add(new int[]{m, n});
+                }
+                else if (grid[m][n] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        int time = 0;
+        while (fresh > 0 && !q.isEmpty()) {
+            time++;
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                int[] rusty = q.poll();
+
+                int a = rusty[0];
+                int b = rusty[1];
+
+                if (a - 1 >= 0 && grid[a - 1][b] == 1) {
+                    grid[a - 1][b] = 2;
+                    fresh--;
+                    q.add(new int[]{a - 1, b});
+                }
+
+                if (a + 1 < l && grid[a + 1][b] == 1) {
+                    grid[a + 1][b] = 2;
+                    fresh--;
+                    q.add(new int[]{a + 1, b});
+                }
+
+                if (b - 1 >= 0 && grid[a][b - 1] == 1) {
+                    grid[a][b - 1] = 2;
+                    fresh--;
+                    q.add(new int[]{a, b - 1});
+                }
+
+                if (b + 1 < w && grid[a][b + 1] == 1) {
+                    grid[a][b + 1] = 2;
+                    fresh--;
+                    q.add(new int[]{a, b + 1});
+                }
+
+
+            }
+        }
+
+        if (fresh > 0) {
+            return -1;
+        } else {
+            return time;
+        }
+
+    }
+
+    //lc 42
+    public int trap(int[] height) {
+        if (height == null || height.length == 0){
+            return 0;
+        }
+        int sum = 0;
+        int size = height.length;
+        int[] left_max = new int[size];
+        int[] right_max = new int[size];
+
+        left_max[0] = height[0];
+        right_max[size - 1] = height[size - 1];
+
+        for (int i = 1; i < size; i++){
+            left_max[i] = Math.max(height[i],height[i-1]);
+        }
+
+        for (int j = size - 2; j >=0; j--){
+            right_max[j] = Math.max(height[j], height[j+1]);
+        }
+
+        for (int m = 0; m < size; m++){
+            sum += Math.max(left_max[m], right_max[m]) - height[m];
+        }
+
+        return sum;
+    }
+
+
+
 
 
 }

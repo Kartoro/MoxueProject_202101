e3f81a72d1c7e591312afa6a0abda54826c3835b
Bangjing Yang <962391470@qq.com>
2021-03-12 11:44:18 +0800

    Update 0307.py
    LC 21

--- mxc006_homework/0307.py
+++ mxc006_homework/0307.py
@@ -1,4 +1,5 @@
 # LC 21
+# 自己 迭代
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         left = l1
@@ -9,11 +10,11 @@ class Solution:
         while left and right:
             if left.val >= right.val:
                 temp = right.next
-                print('temp', temp)
+            
                 right.next = None
                 cur.next = right
                 cur = cur.next
-                print('res', res)
+
                 right = temp
             else:
                 temp = left.next
@@ -28,3 +29,41 @@ class Solution:
             cur.next = left
             
         return res.next
+
+# 题解 迭代 优化
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        left = l1
+        right = l2
+
+        res = ListNode(0)
+        cur = res
+        while left and right:
+            if left.val >= right.val:
+                cur.next = right
+                right = right.next
+            else:
+                cur.next = left
+                left = left.next
+            cur = cur.next
+        
+        if not left:
+            cur.next = right
+        elif not right:
+            cur.next = left
+            
+        return res.next
+# 题解 递归
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
